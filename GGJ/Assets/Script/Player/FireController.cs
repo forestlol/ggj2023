3a45d479868dfb96e272b28dea45ee2f7f4ca9d6
forestlol <wdagonstar@hotmail.com>
2023-02-03 10:51:55 +0800

    + Removed duplicate file + Added a default weapon for testing

--- GGJ/Assets/Script/Player/FireController.cs
+++ GGJ/Assets/Script/Player/FireController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,17 +15,35 @@ public class FireController : MonoBehaviour
 
     [Space]
 
-    [Header("Bullet Statistics")]
-    [SerializeField, Range(0.05f, 1f)]
-    float fireRate;
-    [SerializeField, Range(1,10)]
-    int bulletDamage;
-    [SerializeField, Range(1,10)]
-    int bulletSpeed;
+    [Header("Current Weapon")]
+    [SerializeField]
+    Weapon currentWeapon;
+
+    [Space(10)]
+
+    [Header("List of Weapons")]
+    [SerializeField]
+    List<Weapon> weapons;
 
     Rigidbody bullet;
     bool canFire = true;
 
+    Dictionary<string, Weapon> weaponDict = new Dictionary<string, Weapon>();
+
+    private void Start()
+    {
+        AddWeaponToDictionary();
+    }
+
+    void AddWeaponToDictionary()
+    {
+        for (int i = 0; i < weapons.Count; ++i)
+        {
+            weaponDict[weapons[i].weaponName] = weapons[i];
+        }
+
+        currentWeapon = weaponDict[weapons[0].weaponName];
+    }
     // Update is called once per frame
     void Update()
     {
@@ -41,10 +60,28 @@ public class FireController : MonoBehaviour
     {
         canFire = false;
         bullet = Instantiate(m_bullet, spawnPoint.position, spawnPoint.rotation);
-        bullet.AddForce(spawnPoint.forward * bulletSpeed, ForceMode.Impulse);
-        bullet.GetComponent<Bullet>().SpawnBullet(bulletDamage);
+        bullet.AddForce(spawnPoint.forward * currentWeapon.bulletSpeed, ForceMode.Impulse);
+        bullet.GetComponent<Bullet>().SpawnBullet(currentWeapon.bulletDamage);
 
-        yield return new WaitForSeconds(fireRate);
+        yield return new WaitForSeconds(currentWeapon.fireRate);
         canFire = true;
     }
+
+    public void GetWeaponType(string weaponName)
+    {
+        currentWeapon = weaponDict[weaponName];
+    }
+}
+
+[Serializable]
+public class Weapon
+{
+    [Header("Bullet Statistics")]
+    public string weaponName;
+    [Range(0.05f, 1f)]
+    public float fireRate;
+    [Range(1, 10)]
+    public int bulletDamage;
+    [Range(1, 10)]
+    public int bulletSpeed;
 }
