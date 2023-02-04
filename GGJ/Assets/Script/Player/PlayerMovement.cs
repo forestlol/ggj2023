8a4d975f9627c23905f05cb18bf62a3285079f35
VauserVonShepherd <vauservonshepherd@yahoo.com.sg>
2023-02-04 23:27:35 +0800

    Closest Target

--- GGJ/Assets/Script/Player/PlayerMovement.cs
+++ GGJ/Assets/Script/Player/PlayerMovement.cs
@@ -17,6 +17,9 @@ public class PlayerMovement : Movement
     RaycastHit hitPosition;
     float angle; 
 
+    [SerializeField]
+    float focusRange = 10;
+
     // Start is called before the first frame update
     new void Start()
     {
@@ -30,6 +33,20 @@ public class PlayerMovement : Movement
         moveX = Input.GetAxis("Horizontal");
         moveZ = Input.GetAxis("Vertical");
 
+        if(FocusOnClosestEnemy(focusRange)){
+            return;
+        }
+
+        FocusOnMousePointer();
+    }
+
+    private void FixedUpdate()
+    {
+        Move(moveX, moveZ);
+    }
+
+    void FocusOnMousePointer()
+    {
         ray = cam.ScreenPointToRay(Input.mousePosition);
 
         bool hit = Physics.Raycast(ray, out hitPosition, Mathf.Infinity, hitLayer);
@@ -40,11 +57,32 @@ public class PlayerMovement : Movement
         lookDir = hitPosition.point - transform.position;
         lookDir.y = 0;
         transform.forward = lookDir;
-        
     }
 
-    private void FixedUpdate()
+    bool FocusOnClosestEnemy(float radius)
     {
-        Move(moveX, moveZ);
+        LayerMask worldLayer = LayerMask.NameToLayer("Enemy");
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius, 1 << worldLayer);
+        
+        if(hitColliders.Length == 0)
+        {
+            return false;
+        }
+
+        Collider closest = hitColliders[0];
+
+        foreach (Collider hitCollider in hitColliders)
+        {
+            if(Vector3.Distance(hitCollider.transform.position, transform.position) < Vector3.Distance(closest.transform.position, transform.position))
+            {
+                closest = hitCollider;
+            }
+        }
+
+        lookDir = closest.transform.position- transform.position;
+        lookDir.y = 0;
+        transform.forward = lookDir;
+
+        return true;
     }
 }
